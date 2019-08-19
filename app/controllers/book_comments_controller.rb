5cd2705585a04ca55499c3aa618156a39ff162b7
山本直樹 <yamamotonaoki@ny-mbp.local>
2019-08-19 13:52:11 +0900

    [Add] レスポンシブ・コメント（投稿、編集、削除）機能追加

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,36 @@
+class BookCommentsController < ApplicationController
+before_action :authenticate_user!
+
+   def create
+     book = Book.find(params[:book_id])
+     comment = current_user.book_comments.new(book_comment_params)
+     comment.book_id = book.id
+     comment.save
+     redirect_to book_path(book)
+   end
+
+   def edit
+     @book = Book.find(params[:book_id])
+     @book_comment = BookComment.find(params[:id])
+   end
+
+   def update
+     book = Book.find(params[:book_id])
+     @comment = BookComment.find(params[:id])
+     @comment.update(book_comment_params)
+     redirect_to book_path(book)
+   end
+
+   def destroy
+      book = Book.find(params[:book_id])
+      @comment = BookComment.find(params[:id])
+      @comment.destroy
+      redirect_to book_path(book)
+   end
+
+
+private
+def book_comment_params
+  params.require(:book_comment).permit(:comment, :user_id, :book_id)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'home#about'
   get 'home/index'
   get 'home/about'
-  resources :books
   resources :users, only: [:show, :edit, :update, :index, :new]
+  resources :books do
+    resources :book_comments, only: [:create, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
