523801bb02cfee43284c83d0156d93bafe69eaaa
山本直樹 <yamamotonaoki@ny-mbp.local>
2019-08-21 16:27:14 +0900

    [Add] Follow・Follower一覧画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  
+  def following
+    @user = User.find(params[:id])
+    # @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    # @users = @user.followers
+    render 'show_follower'
+  end
 
   def index
     @users = User.all
